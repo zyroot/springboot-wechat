d1f16edb360b60859f52ede363823dfdc3ea408e
伟大的探索者 <43232923+zyroot@users.noreply.github.com>
2018-11-09 16:10:17 +0800

    Update RedirectUrlUtil.java

--- src/main/java/com/eim/utils/RedirectUrlUtil.java
+++ src/main/java/com/eim/utils/RedirectUrlUtil.java
@@ -15,9 +15,9 @@ public class RedirectUrlUtil {
     private  WeChatProperties weChatProperties;
 
     //微信授权，获取code（一般不变）
-    private final String url = "http://wechat.ei-marketing.net/springboot-wechat-0.0.1-SNAPSHOT/wechat/authorize.do";
+    private final String url = "http://wechat.ei-marketing.net/springboot-wechat-zy/wechat/authorize.do";
     //根据code 获取WxMpOAuth2AccessToken 跳转获取对象信息（一般不变）
-    private final String param = "url=http://wechat.ei-marketing.net/springboot-wechat-0.0.1-SNAPSHOT/wechat/userInfo.do&";
+    private final String param = "url=http://wechat.ei-marketing.net/springboot-wechat-zy/wechat/userInfo.do&";
     //state传递的参数  我这里是从服务器获取对象后，重定向项目的路径（根据项目改变）
     private final String state = "state=http://192.168.160.1:8080/wechat/get.do";
 
